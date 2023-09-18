8bba1e428c58d9f52295a0f310d6a6e5f869137d
eunseo <kooes2003@naver.com>
2023-09-18 18:40:23 +0900

    [0918]

--- week01/example01.cpp
+++ week01/example01.cpp
@@ -8,10 +8,49 @@
 using namespace std;
 
 int N;
-char board[64][64];  // 띄어쓰기 없이 들어오는 입력이므로
-                     // char형으로 한 글자씩 입력받는 것이 매우 유리함
-
-int main() {
+char board[64][64]; // 띄어쓰기 없이 들어오는 입력이므로
+                    // char형으로 한 글자씩 입력받는 것이 매우 유리함
+void check(int r, int c, int len)
+{
+  if (len == 2)
+  {
+    printf("(");
+    for (int i = r; i < len; i++)
+    {
+      for (int j = r; j < len; j++)
+      {
+        if (board[r][c] != board[i][j])
+        {
+          printf("%c", board[i][j]);
+        }
+      }
+    }
+    if (board[r][c] == '1')
+      printf("%c", '1');
+    if (board[r][c] == '0')
+      printf("%c", '0');
+    printf(")");
+  }
+  for (int i = r; i < len; i++)
+  {
+    for (int j = r; j < len; j++)
+    {
+      if (board[i][j] != board[r][c])
+      {
+        check(r, c, len / 2);
+        check(r, c + len / 2, len / 2);
+        check(r + len / 2, c, len / 2);
+        check(r + len / 2, c + len / 2, len / 2);
+      }
+    }
+  }
+  if (board[r][c] == '1')
+    printf("%c", '1');
+  if (board[r][c] == '0')
+    printf("%c", '0');
+}
+int main()
+{
   /* 궁금하신 분들은 찾아 보시면 좋습니다
    * printf, scanf와 같은 c의 stdio와 stream동기화 해제하는 코드
    * cin, cout간의 순서 연결을 해제시키는 코드
@@ -21,11 +60,13 @@ int main() {
   cout.tie(NULL);
 
   cin >> N;
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
+  for (int i = 0; i < N; i++)
+  {
+    for (int j = 0; j < N; j++)
+    {
       cin >> board[i][j];
     }
   }
-
+  check(0, 0, N);
   return 0;
 }
