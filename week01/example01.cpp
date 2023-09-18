5e3ce1c469b41ed89ac2e423d9cb9d03d31f1da7
eunseo <kooes2003@naver.com>
2023-09-18 18:57:12 +0900

    [0918]

--- week01/example01.cpp
+++ week01/example01.cpp
@@ -12,35 +12,19 @@ char board[64][64]; // 띄어쓰기 없이 들어오는 입력이므로
                     // char형으로 한 글자씩 입력받는 것이 매우 유리함
 void check(int r, int c, int len)
 {
-  if (len == 2)
-  {
-    printf("(");
-    for (int i = r; i < len; i++)
-    {
-      for (int j = r; j < len; j++)
-      {
-        if (board[r][c] != board[i][j])
-        {
-          printf("%c", board[i][j]);
-        }
-      }
-    }
-    if (board[r][c] == '1')
-      printf("%c", '1');
-    if (board[r][c] == '0')
-      printf("%c", '0');
-    printf(")");
-  }
+
   for (int i = r; i < len; i++)
   {
     for (int j = r; j < len; j++)
     {
-      if (board[i][j] != board[r][c])
+      if (board[r][c] != board[i][j])
       {
+        cout << '(';
         check(r, c, len / 2);
         check(r, c + len / 2, len / 2);
         check(r + len / 2, c, len / 2);
         check(r + len / 2, c + len / 2, len / 2);
+        cout << ')';
       }
     }
   }
