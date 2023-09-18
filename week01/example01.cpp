533677bade486b7b7d62339ce988c172e6df6155
Subin Choi <starbin7788@gmail.com>
2023-09-18 18:32:34 +0900

    0918

--- week01/example01.cpp
+++ week01/example01.cpp
@@ -6,9 +6,7 @@
 #include <utility>
 #include <vector>
 using namespace std;
-/*
-che
-*/
+
 int N;
 char board[64][64];  // 띄어쓰기 없이 들어오는 입력이므로
                      // char형으로 한 글자씩 입력받는 것이 매우 유리함
@@ -38,10 +36,6 @@ void check(int r,int c,int len){
   return;
 }
 int main() {
-  /* 궁금하신 분들은 찾아 보시면 좋습니다
-   * printf, scanf와 같은 c의 stdio와 stream동기화 해제하는 코드
-   * cin, cout간의 순서 연결을 해제시키는 코드
-   */
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
