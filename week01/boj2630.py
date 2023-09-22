87ce2e4fe5acdeb3d2a85e0f2501230f580667c4
Jeong-Rae <kkwjdfo@gmail.com>
2023-09-22 19:50:46 +0900

    [0922]

--- week01/boj2630.py
+++ week01/boj2630.py
@@ -8,8 +8,12 @@ import math
 """ 이 함수의 기능
 길이를 받아서 그 길이만큼 종이가 한 색상으로 이루어졌는지 확인하는 함수
 """
+
 B = 0
 W = 0
+n = int(input())
+board = [list(map(int, sys.stdin.readline().split())) for i in range(n)]
+print(board)
 
 
 def check_color(r, c, size, board):
@@ -17,10 +21,10 @@ def check_color(r, c, size, board):
     for i in range(r, r + size):
         for j in range(c, c + size):
             if board[r][c] != board[i][j]:
-                check_color(r, c, size, board)
-                check_color(r, c + size/2, size, board)
-                check_color(r + size/2, c, size, board)
-                check_color(r + size/2, c + size/2, size, board)
+                check_color(r, c, size//2, board)
+                check_color(r, c + size//2, size//2, board)
+                check_color(r + size//2, c, size//2, board)
+                check_color(r + size//2, c + size//2, size//2, board)
                 return
     # 여기 오려면 반복문에 return 을 안 만나야 하고
     # 결국 종이가 하나로 이루어져있다
@@ -30,9 +34,6 @@ def check_color(r, c, size, board):
         B += 1
 
 
-n = int(input())
-board = [list(map(int, sys.stdin.readline().split())) for i in range(n)]
-print(board)
 check_color(0, 0, n, board)
 print(B)
 print(W)
