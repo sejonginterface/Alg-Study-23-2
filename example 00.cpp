43548ee1c33daccba6326986c94151453ba62a52
Youngwoo Cho <cyw5066@naver.com>
2023-09-25 17:09:04 +0900

    add files on my branch

--- example 00.cpp
+++ example 00.cpp
@@ -7,38 +7,62 @@ int answer;
 int arr[10000];
 
 /* 배열에 랜덤하게 숫자를 할당시킨다. 중복된 숫자는 존재하지 않는다.*/
-void set() {
+void set()
+{
   srand((unsigned int)time(NULL));
   missing = rand() % 10000;
-  for (int i = 0; i < 10000; i++) {
-    if (i == missing) continue;
+  for (int i = 0; i < 10000; i++) // Random Number Allocation
+  {
+    if (i == missing)
+      continue;
     arr[i] = i;
   }
-  for (int i = 0; i < 5000; i++) {
+  for (int i = 0; i < 5000; i++) // Shuffle Random Number Index
+  {
     int front = rand() % 10000;
     int back = rand() % 10000;
     int temp;
     temp = arr[front];
     arr[front] = arr[back];
-    arr[back] = arr[front];
+    arr[back] = temp;
   }
 }
 
-/* 2차 시간에 빠진 숫자를 찾는다 */
-int findMissingNumberDouble() {
+/* 2차 시간에 빠진 숫자를 찾는다 == 중첩 반복문 반드시 사용*/
+int findMissingNumberDouble()
+{
+  int res; // DO NOT REMOVE
+
+  int mem[10000] = {0}; // bool로 사용
+  int i;
+
+  for (i = 0; i < 10000; i++)
+    mem[arr[i]] = 1;
+
+  for (i = 0; i < 10000; i++)
+  {
+    if (mem[arr[i]])
+      continue;
+    else
+    {
+      res = arr[i];
+      break;
+    }
+  }
+
+  return res; // DO NOT REMOVE
+}
+
+/* 선형 시간에 빠진 숫자를 찾는다 == 중첩 아닌 반복문 반드시 사용*/
+int findMissingNumberLinear()
+{
   int res;
 
   return res;
 }
 
-/* 선형 시간에 빠진 숫자를 찾는다 */
-int findMissingNumberLinear() {
-  int res;
-
-  return res;
-}
-
-int main() {
+int main()
+{
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
@@ -47,7 +71,7 @@ int main() {
   double duration;
 
   set();
-  start = clock();
+  start = clock(); // Mac의 경우 컴파일 에러 가능성, 오류 뜨면 지워버려라!
   answer = findMissingNumberDouble();
   end = clock();
   duration = (double)(end - start) / CLOCKS_PER_SEC;
@@ -55,7 +79,7 @@ int main() {
   cout << (missing == answer ? "correct" : "wrong") << '\n';
 
   set();
-  start = clock();
+  start = clock(); // Mac의 경우 컴파일 에러 가능성, 오류 뜨면 지워버려라!
   answer = findMissingNumberLinear();
   end = clock();
   duration = (double)(end - start) / CLOCKS_PER_SEC;
